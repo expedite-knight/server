edc4c415b3b3b7c9820243636052c112bd0b5805
Kai Larson <kailarson@Kais-MBP.carolina.rr.com>
2023-08-13 21:34:44 -0400

    mixing reqs?

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,38 +8,39 @@ const { RES_TYPES } = require("../utils/helper");
 require("dotenv").config();
 
 router.get("/", (req, res) => {
-    res.send({ status: 200, message: "Users endpoint" });
+  res.send({ status: 200, message: "Users endpoint" });
 });
 
 router.use(verifyJwt);
 
 router.get("/details", async (req, res) => {
-    const currentUser = await User.findById(req.user.user_id);
-    res.send({ status: 200, user: currentUser });
+  const currentUser = await User.findById(req.user.user_id);
+  res.send({ status: 200, user: currentUser });
 });
 
 router.post("/location/update", async (req, res) => {
-    const result = await updateLocation(req, res);
+  console.log("USER: ", req?.user);
+  const result = await updateLocation(req, res);
 
-    if (result === RES_TYPES.SUCCESS) {
-        res.send({ status: 204, body: { message: result } });
-    } else if (result === RES_TYPES.UNAUTHORIZED) {
-        res.send({ status: 403, body: { error: result } });
-    } else {
-        res.send({ status: 200, body: { routes: result } });
-    }
+  if (result === RES_TYPES.SUCCESS) {
+    res.send({ status: 204, body: { message: result } });
+  } else if (result === RES_TYPES.UNAUTHORIZED) {
+    res.send({ status: 403, body: { error: result } });
+  } else {
+    res.send({ status: 200, body: { routes: result } });
+  }
 });
 
 router.get("/routes", async (req, res) => {
-    const currentUser = await User.findById(req.user.user_id)
-        .populate("routes")
-        .catch((err) => console.log("User not found"));
+  const currentUser = await User.findById(req.user.user_id)
+    .populate("routes")
+    .catch((err) => console.log("User not found"));
 
-    if (currentUser) {
-        res.send({ status: 200, routes: currentUser.routes });
-    } else {
-        res.send({ status: 404, body: { error: RES_TYPES.NOT_FOUND } });
-    }
+  if (currentUser) {
+    res.send({ status: 200, routes: currentUser.routes });
+  } else {
+    res.send({ status: 404, body: { error: RES_TYPES.NOT_FOUND } });
+  }
 });
 
 module.exports = router;
