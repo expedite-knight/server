9ce5c22ac0ca7770e7640cfddd2ef3a8311bd014
Kai Larson <kailarson@Kais-MacBook-Pro.local>
2023-11-17 17:07:54 -0600

    more logs

--- src/service/mapsService.js
+++ src/service/mapsService.js
@@ -15,7 +15,7 @@ const calculateETA = async (route, currentLocation, offset) => {
     const eta = await axios(
       `https://maps.googleapis.com/maps/api/distancematrix/json?departure_time=now&traffic_model=best_guess&origins=${currentLocation}&destinations=${formattedDestination}&key=${MATRIX_API_KEY}`
     ).then(async (res) => {
-      console.log("MATRIX RES: ", res.data);
+      console.log("MATRIX RES: ", res);
       const fullDistance = res.data.rows[0].elements[0].distance.text;
 
       const convertedDistance = () => {
